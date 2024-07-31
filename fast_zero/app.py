0d72492c917e83c81d3305a3bf8a89d94a1c85f4
samuel <samueljacome>
2024-07-31 16:12:57 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,20 @@
 from http import HTTPStatus
 from fastapi import Depends, FastAPI, HTTPException
-from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
 from fast_zero.database import get_session
 from fast_zero.models import User
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 from fast_zero.security import get_password_hash
+from fastapi.security import OAuth2PasswordRequestForm
+from fast_zero.security import (
+    create_access_token,
+    get_password_hash,
+    verify_password,
+    get_current_user,
+)
+import uvicorn
+
 app = FastAPI()
 
 database = []
@@ -52,21 +60,20 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int, user: UserSchema, session: Session = Depends(get_session), current_user: User = Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if not db_user:
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
 
-    db_user.username = user.username
-    db_user.password = get_password_hash(user.password)
-    db_user.email = user.email
+    current_user.username = user.username
+    current_user.password = get_password_hash(user.password)
+    current_user.email = user.email
     session.commit()
-    session.refresh(db_user)
+    session.refresh(current_user)
 
-    return db_user
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
@@ -82,3 +89,30 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.commit()
 
     return {'message': 'User deleted'}
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password'
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password'
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+#For debugging with breakpoints in VS Code
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 
-
+from fast_zero.security import get_password_hash
 @pytest.fixture
 def client(session):
     def get_session_override():
@@ -38,9 +38,23 @@ def session():
 
 @pytest.fixture
 def user(session):
-    user = User(username='Teste', email='teste@test.com', password='testtest')
+    user = User(
+        username='Teste',
+        email='teste@test.com',
+        password=get_password_hash('testtest'),
+    )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = 'testtest'
+
     return user
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
