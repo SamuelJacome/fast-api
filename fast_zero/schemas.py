ba4f7e9d311d2fa8762c401be362862a4ab56972
samuel <samueljacome>
2024-08-02 15:38:54 -0300

    adicionando Todos

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
-
+from fast_zero.models import TodoState
 
 class Message(BaseModel):
     message: str
@@ -32,3 +32,18 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
